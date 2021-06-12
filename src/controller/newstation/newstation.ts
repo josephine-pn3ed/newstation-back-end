a1c16a5349a4f6b7d9c9934dcbc71b6862e881e8
Dominic Gabriel Lasap <dlasap@dnamicro.com>
2021-06-12 08:57:59 +0800

    added basic methods

--- src/controller/newstation/newstation.ts
+++ src/controller/newstation/newstation.ts
@@ -1,12 +1,29 @@
-import { employeePayload } from "../../model/types";
-import { } from "./types";
-const db = require('../../db')
-
-
+import { ID } from "./types";
+const { showAllEmployees, showAllCompanies } = require("../../model")
 
 module.exports = {
-    showAllEmployees: async (Payload: employeePayload) => {
-        const data = await db.table("Employees").filter({ status: "active" }).run();
-        return data;
+    getAllEmployees: async (id: ID) => {
+        try {
+
+            let data = await showAllEmployees(id);
+
+            return data.length
+                ? { status: "Success! Data Retrieved.", data }
+                : "<center><h2>status: 🤕 ERROR RETRIEVING DATA: DATA NOT FOUND! </h2></center>";
+        } catch (error) {
+            console.log("Error!");
+        }
+    },
+    getAllCompanies: async (id: ID) => {
+        try {
+
+            let data = await showAllCompanies(id);
+
+            return data.length
+                ? { status: "Success! Data Retrieved.", data }
+                : "<center><h2>status: 🤕 ERROR RETRIEVING DATA: DATA NOT FOUND! </h2></center>";
+        } catch (error) {
+            console.log("Error!");
+        }
     },
 };
--- src/model/index.ts
+++ src/model/index.ts
@@ -1,7 +1,22 @@
-import { } from './types'
+import { companyPayload, employeePayload, ID, Table } from './types'
 const db = require('../../db');
 
 module.exports = {
+    showAllEmployees: async (company_id: ID) => {
+        const data = await db.table("Employees").filter({ company_id: company_id, status: "active" }).run();
+        return data;
+    },
+    showAllCompanies: async (company_id: ID) => {
+        const data = await db.table("Companies").filter({ company_id: company_id }).run();
+        return data;
+    },
+    enrolCompany: async (payload: companyPayload) => {
+        const data = await db.table("Companies").insert(payload).run();
+        return data;
+    },
+    enrolEmployee: async (company_id: ID, payload: employeePayload) => {
+        const data = await db.table("Employees").insert(payload).run();
+        return data;
+    },
 
-
-}
+};
