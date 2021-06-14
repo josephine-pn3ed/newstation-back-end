c7ecfcae5820c80818d4eed46c6ccbb2a471afba
JMorre <dna_dev@pop-os.localdomain>
2021-06-15 07:48:35 +0800

    login and register company updated

--- src/controller/Company/index.ts
+++ src/controller/Company/index.ts
@@ -1,14 +1,26 @@
 import { Payload, Id, Email, Password } from '../../model/Company/types';
 import { v4 as uuid_v4 } from 'uuid';
 
-const Company = require('../../model/Company');
+const { getCompany, insertCompany, loginCompany } = require('../../model/Company');
 
 module.exports = {
-  getCompany: async (email: Email, password: Password) => {
+  getCompany: async (id: Id) => {
     try {
-      const data = await Company.getCompany("Companies", email);
-      if (data && (data.company_password === password)) {
+      const data = await getCompany("Companies", id);
+      if (data) {
         return true;
+      } else throw Error;
+    } catch (error) {
+      return false;
+    }
+  },
+  loginCompany: async (payload: Payload) => {
+    try {
+      const { company_email_address, company_password } = payload;
+      const data = await loginCompany("Companies", company_email_address);
+      console.log(data[0].company_password, company_password)
+      if (data && (data[0].company_password === company_password)) {
+        return data;
       } else throw Error;
     } catch (error) {
       return false;
@@ -16,10 +28,11 @@ module.exports = {
   },
   insertCompany: async (payload: Payload) => {
     try {
-      const data = await Company.insertCompany("Companies",
+      const data = await insertCompany("Companies",
         {
           ...payload,
           id: uuid_v4(),
+          company_status: 'Active',
           created_date: new Date().toISOString(),
           updated_date: new Date().toISOString(),
         });
--- src/routes/company.ts
+++ src/routes/company.ts
@@ -1,22 +1,21 @@
 import express = require('express')
 const route = express.Router();
-const { getCompany, insertCompany } = require('../controller/Company');
-
-route.get('/', async (req: express.Request, res: express.Response) => {
-  res.status(404).send(`<center><h1> 💀 Please Try Again. HTTP ERROR 404! <br> 😔 PAGE DOES NOT EXIST!!!! </h1></center>`);
-})
+const { getCompany, insertCompany, loginCompany } = require('../controller/Company');
 
 route.get('/company', async (req: express.Request, res: express.Response) => {
-  const { email, password } = req.params;
-  console.log("Accessed READ route");
-  const result = await getCompany(email, password);
-  console.log(result)
+  const { id } = req.params;
+  const result = await getCompany(id);
   return result ? res.send({ "status": "Success" }) : res.send({ "status": "Failed" })
 })
 
 route.post('/company', async (req: express.Request, res: express.Response) => {
   const result = await insertCompany(req.body);
-  return result ? res.send({ "status": "Success" }) : res.send({ "status": "Failed" })
+  return result ? res.send({ "success": true }) : res.send({ "success": false })
+})
+
+route.post('/company/login', async (req: express.Request, res: express.Response) => {
+  const result = await loginCompany(req.body);
+  return result ? res.send({ "success": true }) : res.send({ "sucess": false })
 })
 
 module.exports = route;
