76026b862c607868ff8125e4ad592692a60d11a4
Dominic Gabriel Lasap <dlasap@dnamicro.com>
2021-06-18 02:24:25 +0800

    added :id on getCompany by id

--- src/controller/Company/index.ts
+++ src/controller/Company/index.ts
@@ -1,22 +1,23 @@
 import { Payload, Id, Email, Password } from '../../model/Company/types';
 import { v4 as uuid_v4 } from 'uuid';
+const { getCompany, insertCompany, getCompanyByEmail, deleteCompany, updateCompany } = require('../../model/Company');
 
-const Company = require('../../model/Company');
 
 module.exports = {
-  getCompany: async (email: Email, password: Password) => {
+  getCompany: async (id: Id) => {
     try {
-      const data = await Company.getCompany("Companies", email);
-      if (data && (data.company_password === password)) {
-        return true;
+      const data = await getCompany("Companies", id);
+      if (data) {
+        return data;
       } else throw Error;
     } catch (error) {
       return false;
     }
   },
+
   insertCompany: async (payload: Payload) => {
     try {
-      const data = await Company.insertCompany("Companies",
+      const data = await insertCompany("Companies",
         {
           ...payload,
           id: uuid_v4(),
@@ -33,7 +34,7 @@ module.exports = {
   },
   updateCompany: async (id: Id, payload: Payload) => {
     try {
-      const data = await Company.updateCompany("Companies", id, {
+      const data = await updateCompany("Companies", id, {
         ...payload,
         updated_at: new Date().toISOString(),
       });
@@ -47,7 +48,7 @@ module.exports = {
   },
   deleteCompany: async (id: Id) => {
     try {
-      const data = await Company.deleteCompany("Companies", id, {
+      const data = await deleteCompany("Companies", id, {
         company_status: "Inactive",
         updated_at: new Date().toISOString(),
       });
@@ -62,7 +63,7 @@ module.exports = {
   loginCompany: async (payload: Payload) => {
     try {
       const { company_email_address, company_password } = payload;
-      const data = await Company.getCompanyByEmail("Companies", company_email_address);
+      const data = await getCompanyByEmail("Companies", company_email_address);
       if (data) {
         if (data[0].company_password === company_password) {
           return { "message": "Login successfully." };
